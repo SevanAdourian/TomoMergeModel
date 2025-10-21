2c0a6fbc7842e3ae626b140bca66d77e37b53189
Pranav Mucharla <mucharlapranav@gmail.com>
2025-10-21 19:06:01 -0400

    Added constructor for MergeMethods class, getters and setters for both classes, and error handling for conf.yaml reading

--- Dataset.py
+++ Dataset.py
@@ -4,9 +4,6 @@ import os, yaml
 class Dataset():
     REGIONAL = 0
     GLOBAL = 1
-    # instance variables here
-
-
     # constructors
     def __init__(self, filePath: str, modelType: int):
 
@@ -35,9 +32,11 @@ class Dataset():
             raise ValueError("Model type must be Dataset.REGIONAL or Dataset.GLOBAL")
         
         # load the conf.yaml file
-        with open("conf.yaml", "r") as file:
-            conf = yaml.safe_load(file)
-        
+        try:
+            with open("conf.yaml", "r") as file:
+                conf = yaml.safe_load(file)
+        except OSError:
+            raise ValueError("conf.yaml not found or not in working directory")
         # return to the user, a Dataset instance depending on the model type and file path in conf.yaml
         if modelType == Dataset.REGIONAL:
             return Dataset(conf["path_to_regional_model"], modelType)
@@ -46,6 +45,25 @@ class Dataset():
 
 
     # getters and setters here
+    def getFilePath(self) -> str:
+        return self.filePath
+
+    def setFilePath(self, path: str):
+        if path is None:
+            raise ValueError("Need to pass in file path")
+        if not isinstance(path, str):
+            raise ValueError("File path must be a string")
+        if not os.path.exists(path):
+            raise ValueError("Incorrect file path: file could not be found")
+        self.filePath = path
+
+    def getModelType(self) -> int:
+        return self.modelType
+
+    def setModelType(self, mt: int):
+        if mt not in [Dataset.REGIONAL, Dataset.GLOBAL]:
+            raise ValueError("Model type must be Dataset.REGIONAL or Dataset.GLOBAL")
+        self.modelType = mt
 
 
     # dataset specific functions here
