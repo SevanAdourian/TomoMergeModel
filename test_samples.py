6968fc557294945ef52083460af83e22ab2dd792
Pranav Mucharla <mucharlapranav@gmail.com>
2025-11-13 17:23:14 -0500

    WIP: Finished tests for file path, model type, and spline path

--- test_samples.py
+++ test_samples.py
@@ -7,18 +7,38 @@ import pytest
 
 # first series of tests: checking for invalid object creation
 
-# bad filePath for Dataset
-def badFilePath_1():
-    temp = Dataset(1, Dataset.GLOBAL)
-
-def badFilePath_2():
-    temp = Dataset(None, Dataset.GLOBAL)
-
-def badFilePath_3():
-    temp = Dataset("hello", Dataset.GLOBAL)
-
-def test_badFilePath():
+# bad filePath for Global Dataset
+def test_badFilePath_1():
     with pytest.raises(ValueError):
-        badFilePath_1()
-        badFilePath_2()
-        badFilePath_3()
+        temp = Dataset(1, Dataset.GLOBAL)
+
+def test_badFilePath_2():
+    with pytest.raises(ValueError):
+        temp = Dataset(None, Dataset.GLOBAL)
+
+def test_badFilePath_3():
+    with pytest.raises(ValueError):
+        temp = Dataset("hello", Dataset.GLOBAL)
+
+# bad model type for any Dataset
+def test_badModelType_1():
+    with pytest.raises(ValueError):
+        temp = Dataset("Data\\netcdf\\semucb-2014-ucb-vs.nc", None)
+
+def test_badModelType_2():
+    with pytest.raises(ValueError):
+        temp = Dataset("Data\\netcdf\\semucb-2014-ucb-vs.nc", 3, "Data\\netcdf\\semucb-2014-ucb-vs.nc")
+
+# bad spline file path for regional datasets
+def test_badSplinePath_1():
+    with pytest.raises(ValueError):
+        temp = Dataset("Data\\netcdf\\semucb-2014-ucb-vs.nc", Dataset.REGIONAL, 1)
+
+def test_badSplinePath_2():
+    with pytest.raises(ValueError):
+        temp = Dataset("Data\\netcdf\\semucb-2014-ucb-vs.nc", Dataset.REGIONAL)
+
+def test_badSplinePath_3():
+    with pytest.raises(ValueError):
+        temp = Dataset("Data\\netcdf\\semucb-2014-ucb-vs.nc", Dataset.REGIONAL, "hello")
+
