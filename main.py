4c3800ff9f867e2d1df1450ac2ac8bbe587da4c0
Sevan Jean Sebastien Adourian (adourian) <adourian@sycamore-login2.sm.unc.edu>
2026-03-11 17:41:28 -0400

    Working apply_window function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import multiprocessing
+import cmcrameri.cm as cm
 
 from Dataset import Dataset
 from MergeMethods import MergeMethods
@@ -8,7 +9,7 @@ from ConfigParams import ConfigParams
 
 
 def main() -> None:
-    depths = [30, 50]
+    depths = [150]
     global_mod = Dataset(
         "Data/netcdf/glad-m25-vs-0.0-n4.nc",
         Dataset.GLOBAL,
@@ -22,7 +23,7 @@ def main() -> None:
         globalModel=global_mod,
     )
     configParams = ConfigParams(
-        239, 60, 60, (197.5, 230.5), (52.65, 71.55), "spherical"
+        239, 80, 80, (197.5, 230.5), (52.65, 71.55), "spherical"
     )
 
     merger = MergeMethods(regional_mod, global_mod, configParams, "Vs", "vsv")
@@ -32,9 +33,11 @@ def main() -> None:
     merged_mod = merger.merge()
 
     print("Displaying Merged Maps")
+    cmap_seismic = cm.vik
     for depth in merged_mod.getDataset().depth.values:
         merged_mod.plot_all_variables(
-            depth=depth
+            depth=depth,
+            cmap=cmap_seismic
         )
     print("End of Merged Map Displays")
 
